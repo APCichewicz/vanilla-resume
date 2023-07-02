509e75a6fb1a6ea71730484a4fafd2655081df4e
APCichewicz <apcichewicz@gmail.com>
2023-07-02 08:28:21 -0400

    fixed an issue w/ import

--- src/index.js
+++ src/index.js
@@ -1,12 +1,4 @@
-// this script will make a request to an azure function app and take the response and add it to the content of span with id visitor-count
-
-// import the axios library, to make http requests
 import axios from "axios";
-
-// make an axios request to the azure function app
-let count = axios.get(
-  "https://apcvisitorcount.azurewebsites.net/api/ResumeHttpTrigger?"
-);
 
 // get the span element with id visitor-count
 const visitorCountSpan = document.getElementById("visitor-count");
